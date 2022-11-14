1bf50e4273fae125d98a7da99e0f20ed2585098b
Nikolaipe <nikolaipe@getacademy.no>
2022-11-14 15:29:10 +0100

    It's a start I will have to scrap

--- script.js
+++ script.js
@@ -2,3 +2,27 @@
 // push randomNum til array med loop,
 // skriv html med loop,
 // lagre posisjon på tall og push og splice [i] ?
+
+const generatedNumbers = [];
+
+
+//view
+createBoard()
+function createBoard(){
+    let board;
+    for (let i = 0; i < 9; i++){
+        randomGenerator(numGenerated)
+        
+        generatedNumbers.push(numGenerated)
+        console.log(generatedNumbers[i])
+    }
+}
+
+
+//controller
+function randomGenerator(numGenerated){
+    let tempRndNum;
+    tempRndNum = Math.floor(Math.random() * 9) + 1;
+    numGenerated = tempRndNum;
+    return numGenerated;
+}
