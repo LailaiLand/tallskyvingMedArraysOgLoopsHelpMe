c8558e547a3183165be7d5a58cb7f95611a2a802
Nikolailai <nikpeder@gmail.com>
2022-11-15 13:05:10 +0100

    Only works
    Måtte se litt på løsningsforslaget igjen..

--- script.js
+++ script.js
@@ -3,26 +3,67 @@
 // skriv html med loop,
 // lagre posisjon på tall og push og splice [i] ?
 
-const generatedNumbers = [];
+let generatedNumbers = [];
+let randomNum = 0;
+let innhold = document.getElementById('app');
 
 
 //view
-createBoard()
-function createBoard(){
-    let board;
-    for (let i = 0; i < 9; i++){
-        randomGenerator(numGenerated)
-        
-        generatedNumbers.push(numGenerated)
-        console.log(generatedNumbers[i])
+function updateBoard(){
+    innhold.innerHTML = '';
+    for (i = 0; i < generatedNumbers.length; i++){
+        innhold.innerHTML += `
+        <div class="rute" onclick="flyttTall(` + i + `)"> ` + generatedNumbers[i] + ` </div>
+        `;
     }
 }
 
 
+
 //controller
-function randomGenerator(numGenerated){
-    let tempRndNum;
-    tempRndNum = Math.floor(Math.random() * 9) + 1;
-    numGenerated = tempRndNum;
-    return numGenerated;
+
+createNumbers()
+function createNumbers(){
+    for (let i = 0; i < 9; i++){
+        randomGenerator();
+        while (generatedNumbers.includes(randomNum) || generatedNumbers.includes['']){
+            randomGenerator();
+        }
+        generatedNumbers.push(randomNum);
+        console.log(generatedNumbers[i]);
+    }
+    updateBoard();
+}
+function randomGenerator(){
+    randomNum = Math.floor(Math.random() * 9) + 1;
+    if (randomNum == 9) randomNum = '';
+    console.log('random er', randomNum)
+
+    return
+}
+function flyttTall(selected){
+    for (let placer of [-3, -1, 1, 3]){
+        let seeker = selected - placer;
+        if (seeker < 0 || seeker > generatedNumbers.length) continue;
+        if (generatedNumbers[seeker] == ''){
+            generatedNumbers[seeker] = generatedNumbers[selected];
+            generatedNumbers[selected] = '';
+        }
+    }
+    updateBoard();
+    victoryCheck();
+}
+function victoryCheck(){
+    let comparisonLast = 0;
+    for (let i = 0; i < generatedNumbers.length - 1; i++){
+       if (generatedNumbers[i] > comparisonLast){
+        comparisonLast = generatedNumbers[i];
+        continue;
+       } else return;
+    }
+    innhold.innerHTML += 'Gratulerer!';
+}
+function juks(){
+    generatedNumbers = [1, 2, 3, 4, 5, 6, 7, '', 8]
+    updateBoard();
 }
